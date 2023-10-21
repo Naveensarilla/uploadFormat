1f0494971f0111e9fb0282a8b253318ec1a3197f
Naveensarilla <naveen.s@egradtutor.in>
2023-10-21 12:17:40 +0530

    uploading

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import './App.css';
+function App() {
+  const [file, setFile] = useState(null);
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+  const handleUpload = () => {
+    const formData = new FormData();
+    formData.append('document', file);
+    fetch('http://localhost:5030/upload', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.text())
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  return (
+    <div className="App">
+      <h1>Document Image Uploader</h1>
+      <input type="file" accept=".docx" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+}
+ 
+export default App;
+
+ 
