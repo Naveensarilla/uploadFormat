5d19b13de2e8bf2f2d520be003996aaa7c074c5d
sindhu-121 <sindhu.g@egradtutor.in>
2023-10-26 11:08:25 +0530

    ddd

--- backend/server.js
+++ backend/server.js
@@ -210,49 +210,109 @@ app.get('/quiz_topics/:unit_id', (req, res) => {
 //   }
 // });
 
-  
 app.post('/upload', upload.single('document'), async (req, res) => {
   const docxFilePath = `uploads/${req.file.filename}`;
   const outputDir = `uploads/${req.file.originalname}_images`;
-  const topic_id= req.body.topic_id;
+  const topic_id = req.body.topic_id;
+
   if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir);
   }
-
-  // Assuming the selected topic IDs are sent in the request body as an array
-  // const selectedTopicIds = req.body.selectedTopicIds;
 
   try {
     const result = await mammoth.convertToHtml({ path: docxFilePath });
     const htmlContent = result.value;
     const $ = cheerio.load(htmlContent);
+    const textResult = await mammoth.extractRawText({ path: docxFilePath });
+    const textContent = textResult.value;
 
-    $('img').each(async function (i, element) {
+    // Split the text into sections based on a delimiter, e.g., paragraph separation.
+    // Assuming paragraphs are separated by double line breaks.
+    const textSections = textContent.split('\n\n');
+
+    // Get all images in the order they appear in the HTML
+    const images = [];
+    $('img').each(function (i, element) {
       const base64Data = $(this).attr('src').replace(/^data:image\/\w+;base64,/, '');
       const imageBuffer = Buffer.from(base64Data, 'base64');
-
-      try {
-        // Insert the image data and the selected topic ID into the image table
-      //   for (const topicId of selectedTopicIds) {
-      //     await connection.query('INSERT INTO images (image_data, topic_id) VALUES (?, ?)', [imageBuffer, topic_id]);
-      //   }
-      await connection.execute('INSERT INTO images (image_data,topic_id) VALUES (?, ?)', [imageBuffer, topic_id]);
-      console.log('Image inserted successfully');
-      } catch (error) {
-          console.error('Error inserting image data:', error);
-        res.status(500).send('Error inserting image data into the database.');
-        return;
-      }
+      images.push(imageBuffer);
     });
 
-    // No need to close the connection here
+    // Save content in the same order, but store every 6th image in a separate table
+    for (let i = 0; i < Math.max(textSections.length, images.length); i++) {
+      if (i < images.length) {
+        if (i % 6 === 0) {
+          // Insert the image data into a new "image_set" table
+          await connection.execute('INSERT INTO 6egquiz_questions(qustion_data, topic_id) VALUES (?, ?)', [images[i], topic_id]);
+          console.log(`Image content ${i} inserted successfully into 6egquiz_questions table`);
+        } else {
+          // Insert the image data into the existing "images" table
+          await connection.execute('INSERT INTO images (image_data, topic_id) VALUES (?, ?)', [images[i], topic_id]);
+          console.log(`Image content ${i} inserted successfully into images table`);
+        }
+      }
+      if (i < textSections.length) {
+        // Insert the text content into the "images" table as per your original code.
+        await connection.execute('INSERT INTO images (content_text, topic_id) VALUES (?, ?)', [textSections[i], topic_id]);
+        console.log(`Text content ${i} inserted successfully into images table`);
+      }
+    }
+// Variable to track the current image index
+let currentImageIndex = 1;
 
-    res.send('Images extracted and saved to the database with selected topic IDs successfully.');
+for (let i = 1; i < Math.max(textSections.length, images.length); i++) {
+if (i < images.length) {
+  if (currentImageIndex >= 1 && currentImageIndex <= 4) {
+    // Insert the image data into a new "image_set" table for the first 4 images
+    await connection.execute('INSERT INTO 7egquiz_options(option_data, topic_id) VALUES (?, ?)', [images[i], topic_id]);
+    console.log(`Image content ${i} inserted successfully into 7egquiz_options table`);
+  } else {
+    // Insert the image data into the existing "images" table for the rest
+    await connection.execute('INSERT INTO images (image_data, topic_id) VALUES (?, ?)', [images[i], topic_id]);
+    console.log(`Image content ${i} inserted successfully into images table`);
+  }
+
+  currentImageIndex += 1; // Increment the current image index
+
+  // After the 4th image, reset the counter to insert images with an increment of 2
+  if (currentImageIndex === 5) {
+    currentImageIndex = 1; // Reset to 1 to repeat the first 4 images
+    i += 2; // Increment i by 2 to skip 2 images
+  }
+}
+if (i < textSections.length) {
+  // Insert the text content into the "images" table as per your original code.
+  await connection.execute('INSERT INTO images (content_text, topic_id) VALUES (?, ?)', [textSections[i], topic_id]);
+  console.log(`Text content ${i} inserted successfully into images table`);
+}
+}
+
+
+for (let i = 5; i < Math.max(textSections.length, images.length); i++) {
+if (i < images.length) {
+  if (i % 5 === 0) {
+    // Insert the image data into a new "image_set" table
+    await connection.execute('INSERT INTO egquiz_solution (solustion_data, topic_id) VALUES (?, ?)', [images[i], topic_id]);
+    console.log(`Image content ${i} inserted successfully into solustion table`);
+  } else {
+    // Insert the image data into the existing "images" table
+    await connection.execute('INSERT INTO images (image_data, topic_id) VALUES (?, ?)', [images[i], topic_id]);
+    console.log(`Image content ${i} inserted successfully into images table`);
+  }
+}
+if (i < textSections.length) {
+  // Insert the text content into the "images" table as per your original code.
+  await connection.execute('INSERT INTO images (content_text, topic_id) VALUES (?, ?)', [textSections[i], topic_id]);
+  console.log(`Text content ${i} inserted successfully into images table`);
+}
+}
+    res.send('Text content and images extracted and saved to the database with the selected topic ID successfully.');
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error extracting images and saving to the database.');
+    res.status(500).send('Error extracting content and saving it to the database.');
   }
 });
+ 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
